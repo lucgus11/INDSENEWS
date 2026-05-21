f2d4e5b30aab74166f7b0b07cd8176d4830f9782
lucgus11 <lucas.gustin@indse.be>
2026-05-21 10:54:14 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,32 @@
+self.addEventListener('push', event => {
+  let data = { title: 'INDSENEWS', body: 'Nouvelle alerte info !', url: '/' };
+  
+  if (event.data) {
+    try {
+      data = event.data.json();
+    } catch (e) {
+      data.body = event.data.text();
+    }
+  }
+
+  const options = {
+    body: data.body,
+    icon: '/icons/icon-192.png',
+    badge: '/icons/icon-192.png',
+    vibrate: [200, 100, 200],
+    data: {
+      url: data.url || '/'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, options)
+  );
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url)
+  );
+});
